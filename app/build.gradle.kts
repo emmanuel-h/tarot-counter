73abea17632bfdcdc4d18dbc2182ce4dc6786dac
Emmanuel <emmanuel-h@mailbox.org>
2026-04-06 17:51:44 +0200

    chore: bump version to 1.1 (code 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "fr.mandarine.tarotcounter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
